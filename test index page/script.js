7c0fb1af88611317d90629c03fff6223c652abe3
Ivoslav <ivoslavpetkov@gmail.com>
2024-03-18 16:23:56 +0200

    Now saves rooms to local storage

--- test index page/script.js
+++ test index page/script.js
@@ -1,5 +1,4 @@
 function startConnect(){
-
     clientID = "clientID - "+parseInt(Math.random() * 100);
 
     host = "test.mosquitto.org";   
@@ -20,7 +19,100 @@ function startConnect(){
         userName: userId,
         password: password
     });
+
+    displayRoomsFromLocalStorage(); // Display rooms from local storage
 }
+
+function addNewRoom() {
+    var roomName = prompt("Enter the name of the new room:");
+    if (roomName) {
+        var newRoomDiv = document.createElement("div");
+        newRoomDiv.classList.add("room");
+
+        var roomHeading = document.createElement("h2");
+        roomHeading.textContent = roomName;
+
+        var roomButton = document.createElement("button");
+        roomButton.classList.add("lightBtn");
+        roomButton.textContent = "Turn On";
+        roomButton.setAttribute("onclick", "publishMessage('" + roomName.toLowerCase().replace(/\s/g, '') + "')");
+
+        var removeButton = document.createElement("button");
+        removeButton.classList.add("lightBtn");
+        removeButton.textContent = "Remove";
+        removeButton.setAttribute("onclick", "removeRoom(this)");
+
+        newRoomDiv.appendChild(roomHeading);
+        newRoomDiv.appendChild(roomButton);
+        newRoomDiv.appendChild(removeButton);
+
+        var roomsContainer = document.getElementById("roomsContainer");
+        roomsContainer.insertBefore(newRoomDiv, roomsContainer.lastElementChild);
+
+        client.subscribe(roomName.toLowerCase().replace(/\s/g, ''));
+        console.log("New room '" + roomName + "' added.");
+
+        // Save to local storage
+        saveRoomToLocalStorage(roomName);
+    }
+}
+
+function saveRoomToLocalStorage(roomName) {
+    var rooms = JSON.parse(localStorage.getItem("rooms")) || [];
+    rooms.push(roomName);
+    localStorage.setItem("rooms", JSON.stringify(rooms));
+}
+
+function displayRoomsFromLocalStorage() {
+    var rooms = JSON.parse(localStorage.getItem("rooms")) || [];
+    rooms.forEach(function(roomName) {
+        addRoomFromLocalStorage(roomName);
+    });
+}
+
+function addRoomFromLocalStorage(roomName) {
+    var newRoomDiv = document.createElement("div");
+    newRoomDiv.classList.add("room");
+
+    var roomHeading = document.createElement("h2");
+    roomHeading.textContent = roomName;
+
+    var roomButton = document.createElement("button");
+    roomButton.classList.add("lightBtn");
+    roomButton.textContent = "Turn On";
+    roomButton.setAttribute("onclick", "publishMessage('" + roomName.toLowerCase().replace(/\s/g, '') + "')");
+
+    var removeButton = document.createElement("button");
+    removeButton.classList.add("lightBtn");
+    removeButton.textContent = "Remove";
+    removeButton.setAttribute("onclick", "removeRoom(this)");
+
+    newRoomDiv.appendChild(roomHeading);
+    newRoomDiv.appendChild(roomButton);
+    newRoomDiv.appendChild(removeButton);
+
+    var roomsContainer = document.getElementById("roomsContainer");
+    roomsContainer.insertBefore(newRoomDiv, roomsContainer.lastElementChild);
+}
+
+function removeRoom(button) {
+    var roomToRemove = button.parentNode;
+    roomToRemove.parentNode.removeChild(roomToRemove);
+    console.log("Room removed.");
+
+    var roomName = roomToRemove.querySelector("h2").textContent;
+    removeRoomFromLocalStorage(roomName);
+}
+
+function removeRoomFromLocalStorage(roomName) {
+    var rooms = JSON.parse(localStorage.getItem("rooms")) || [];
+    var index = rooms.indexOf(roomName);
+    if (index !== -1) {
+        rooms.splice(index, 1);
+        localStorage.setItem("rooms", JSON.stringify(rooms));
+    }
+}
+
 
 function onConnectionLost(responseObject){
     console.log("ERROR: Connection is lost.");
@@ -53,42 +145,4 @@ function publishMessage(message){
 
     client.send(Message);
     console.log("Message to topic "+topic+" is sent");
-}
-
-function addNewRoom() {
-    var roomName = prompt("Enter the name of the new room:");
-    if (roomName) {
-        var newRoomDiv = document.createElement("div");
-        newRoomDiv.classList.add("room");
-
-        var roomHeading = document.createElement("h2");
-        roomHeading.textContent = roomName;
-
-        var roomButton = document.createElement("button");
-        roomButton.classList.add("lightBtn");
-        roomButton.textContent = "Turn On";
-        roomButton.setAttribute("onclick", "publishMessage('" + roomName.toLowerCase().replace(/\s/g, '') + "')");
-
-        var removeButton = document.createElement("button");
-        removeButton.classList.add("lightBtn");
-        removeButton.textContent = "Remove";
-        removeButton.setAttribute("onclick", "removeRoom(this)");
-
-        newRoomDiv.appendChild(roomHeading);
-        newRoomDiv.appendChild(roomButton);
-        newRoomDiv.appendChild(removeButton);
-
-        var addNewRoomButton = document.getElementById("addNewRoom").parentNode;
-        addNewRoomButton.parentNode.insertBefore(newRoomDiv, addNewRoomButton);
-
-        client.subscribe(roomName.toLowerCase().replace(/\s/g, ''));
-        console.log("New room '" + roomName + "' added.");
-    }
-}
-
-
-function removeRoom(button) {
-    var roomToRemove = button.parentNode;
-    roomToRemove.parentNode.removeChild(roomToRemove);
-    console.log("Room removed.");
 }
