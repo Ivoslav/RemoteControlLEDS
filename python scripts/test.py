0afcd90bb202f7ce3425bc2f5ad778e4c0bd0622
Ivoslav <ivoslavpetkov@gmail.com>
2024-03-26 21:44:52 +0200

    update python

--- python scripts/test.py
+++ python scripts/test.py
@@ -1,17 +1,5 @@
-# import paho.mqtt.client as mqtt
-# broker_address="test.mosquitto.org"
-# print("creating new instance")
-# client = mqtt.Client()
-# print("connecting to broker")
-# client.connect(broker_address)
-# print("Subscribing to topic","ivan")
-# client.subscribe("ivan")
-# print("Publishing message to topic","ivan")
-# client.publish("ivan","OFF")
-
 import paho.mqtt.client as mqtt
 
-# Callback function to handle when a message is received
 def on_message(client, userdata, message):
     print("Received message:", str(message.payload.decode("utf-8")))
 
