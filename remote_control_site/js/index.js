0fa016f3120e41b929dbb3141c63f2565e655eb8
Ivoslav <ivoslavpetkov@gmail.com>
2024-05-20 00:00:43 +0300

    fixed bug

--- remote_control_site/js/index.js
+++ remote_control_site/js/index.js
@@ -1,15 +1,4 @@
 var roomName;
-const userPasswordEl = document.querySelector("#password");
-const togglePasswordEl = document.querySelector("#togglePassword");
-
-togglePasswordEl.addEventListener("click", function () {
-  if (this.checked === true) {
-    userPasswordEl.setAttribute("type", "text");
-  } else {
-    userPasswordEl.setAttribute("type", "password");
-  }
-});
-
 
 function addNewRoom() {
     roomName = prompt("Въведете името на новата стая:");
--- remote_control_site/login.php
+++ remote_control_site/login.php
@@ -79,7 +79,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   <link rel="stylesheet" href="./css/main.css">
   <link rel="shortcut icon" href="./img/favicon-16x16.png" type="image/x-icon">
-  <script defer src="./js/index.js"></script>
+  <script defer src="./js/script.js"></script>
   <style>
     .dark-mode {
       background-color: #212529;
